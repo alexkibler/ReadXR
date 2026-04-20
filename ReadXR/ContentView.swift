db2b4c91ca372163254c0b091d29b59bc0036572
Alex Kibler <akibler16@gmail.com>
2026-04-19 22:13:58 -0400

    Initial Commit

---
+++ ReadXR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReadXR
+//
+//  Created by Alex Kibler on 4/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReadXR/ReadXRApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadXRApp.swift
+//  ReadXR
+//
+//  Created by Alex Kibler on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct ReadXRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
